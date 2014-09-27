a9c6931cb434d2d2ab2ed8c5c2e274f894edb08d
Greg Einfrank <greg.einfrank@gmail.com>
2014-09-27 13:14:06 -0400

    Start contacts app

--- app/src/main/java/com/example/greegles/omgandroid/JSONAdapter.java
+++ app/src/main/java/com/example/greegles/omgandroid/JSONAdapter.java
@@ -8,8 +8,6 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -17,8 +15,6 @@ import org.json.JSONObject;
  * Created by greegles on 9/26/14.
  */
 public class JSONAdapter extends BaseAdapter {
-
-    private static final String IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/";
 
     Context mContext;
     LayoutInflater mInflater;
@@ -54,13 +50,15 @@ public class JSONAdapter extends BaseAdapter {
         if (convertView == null) {
 
             // Inflate the custom row layout from your XML.
-            convertView = mInflater.inflate(R.layout.row_book, null);
+            convertView = mInflater.inflate(R.layout.row_contact, null);
 
             // create a new "Holder" with subviews
             holder = new ViewHolder();
             holder.thumbnailImageView = (ImageView) convertView.findViewById(R.id.img_thumbnail);
-            holder.titleTextView = (TextView) convertView.findViewById(R.id.text_title);
-            holder.authorTextView = (TextView) convertView.findViewById(R.id.text_author);
+            holder.nameTextView = (TextView) convertView.findViewById(R.id.text_name);
+            holder.phoneTextView = (TextView) convertView.findViewById(R.id.text_phone);
+            holder.emailTextView = (TextView) convertView.findViewById(R.id.text_email);
+
 
             // hang onto this holder for future recyclage
             convertView.setTag(holder);
@@ -71,41 +69,28 @@ public class JSONAdapter extends BaseAdapter {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        // Get the current book's data in JSON form
         JSONObject jsonObject = (JSONObject) getItem(position);
 
-        // See if there is a cover ID in the Object
-        if (jsonObject.has("cover_i")) {
+        String contactName = "";
+        String contactPhone = "";
+        String contactEmail = "";
 
-            // If so, grab the Cover ID out from the object
-            String imageID = jsonObject.optString("cover_i");
-
-            // Construct the image URL (specific to API)
-            String imageURL = IMAGE_URL_BASE + imageID + "-S.jpg";
-
-            // Use Picasso to load the image
-            // Temporarily have a placeholder in case it's slow to load
-            Picasso.with(mContext).load(imageURL).placeholder(R.drawable.ic_books).into(holder.thumbnailImageView);
-        } else {
-
-            // If there is no cover ID in the object, use a placeholder
-            holder.thumbnailImageView.setImageResource(R.drawable.ic_books);
-        }
-        // Grab the title and author from the JSON
-        String bookTitle = "";
-        String authorName = "";
-
-        if (jsonObject.has("title")) {
-            bookTitle = jsonObject.optString("title");
+        if (jsonObject.has("name")) {
+            contactName = jsonObject.optString("name");
         }
 
-        if (jsonObject.has("author_name")) {
-            authorName = jsonObject.optJSONArray("author_name").optString(0);
+        if (jsonObject.has("phone_number")) {
+            contactPhone = jsonObject.optString("phone_number");
         }
 
+        if (jsonObject.has("email")) {
+            contactEmail = jsonObject.optString("email");
+        }
+        holder.thumbnailImageView.setImageResource(R.drawable.ic_books);
         // Send these Strings to the TextViews for display
-        holder.titleTextView.setText(bookTitle);
-        holder.authorTextView.setText(authorName);
+        holder.nameTextView.setText(contactName);
+        holder.phoneTextView.setText(contactPhone);
+        holder.emailTextView.setText(contactEmail);
         return convertView;
     }
 
@@ -119,7 +104,8 @@ public class JSONAdapter extends BaseAdapter {
     // inflation and finding by ID once ever per View
     private static class ViewHolder {
         public ImageView thumbnailImageView;
-        public TextView titleTextView;
-        public TextView authorTextView;
+        public TextView nameTextView;
+        public TextView phoneTextView;
+        public TextView emailTextView;
     }
 }
--- app/src/main/java/com/example/greegles/omgandroid/MainActivity.java
+++ app/src/main/java/com/example/greegles/omgandroid/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.greegles.omgandroid;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,6 +27,8 @@ import android.widget.Toast;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
@@ -30,8 +36,7 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 
 
-public class MainActivity extends Activity implements View.OnClickListener,
-        AdapterView.OnItemClickListener {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
 
     private static final String PREFS = "prefs";
     private static final String PREF_NAME = "name";
@@ -54,10 +59,6 @@ public class MainActivity extends Activity implements View.OnClickListener,
 
         setContentView(R.layout.activity_main);
         mainTextView = (TextView) findViewById(R.id.main_textview);
-        // 2. Access the Button defined in layout XML
-        // and listen for it here
-        mainButton = (Button) findViewById(R.id.main_button);
-        mainButton.setOnClickListener(this);
         // 3. Access the EditText defined in layout XML
         mainEditText = (EditText) findViewById(R.id.main_edittext);
         // 4. Access the ListView
@@ -74,6 +75,12 @@ public class MainActivity extends Activity implements View.OnClickListener,
 
         // Set the ListView to use the ArrayAdapter
         mainListView.setAdapter(mJSONAdapter);
+
+        try {
+            gatherContacts();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -169,23 +176,6 @@ public class MainActivity extends Activity implements View.OnClickListener,
     }
 
     @Override
-    public void onClick(View view) {
-/*        // Take what was typed into the EditText
-        // and use in TextView
-        mainTextView.setText(mainEditText.getText().toString()
-                + " is learning Android development!");
-        // Also add that value to the list shown in the ListView
-        mNameList.add(mainEditText.getText().toString());
-        mArrayAdapter.notifyDataSetChanged();
-        // 6. The text you'd like to share has changed,
-        // and you need to update
-        setShareIntent();*/
-
-        // 9. Take what was typed into the EditText and use in search
-        queryBooks(mainEditText.getText().toString());
-    }
-
-    @Override
     public void onItemClick(AdapterView parent, View view, int position, long id) {
         // Log the item's position and contents
         // to the console in Debug
@@ -204,6 +194,69 @@ public class MainActivity extends Activity implements View.OnClickListener,
 
         // start the next Activity using your prepared Intent
         startActivity(detailIntent);
+    }
+
+    private void gatherContacts() throws JSONException {
+        String phoneNumber = null;
+        String email = null;
+
+        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String _ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+        Uri EmailCONTENT_URI =  ContactsContract.CommonDataKinds.Email.CONTENT_URI;
+        String EmailCONTACT_ID = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
+        String DATA = ContactsContract.CommonDataKinds.Email.DATA;
+
+        ContentResolver contentResolver = getContentResolver();
+
+        Cursor cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
+        ArrayList<JSONObject> contactList = new ArrayList<JSONObject>();
+        // Loop for every contact in the phone
+        if (cursor.getCount() > 0) {
+
+            while (cursor.moveToNext()) {
+
+                JSONObject contact = new JSONObject();
+
+                String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
+                String name = cursor.getString(cursor.getColumnIndex( DISPLAY_NAME ));
+
+                contact.putOpt("contact_id", contact_id);
+                contact.putOpt("name", name);
+
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex( HAS_PHONE_NUMBER )));
+
+                if (hasPhoneNumber > 0) {
+                    // Query and loop for every phone number of the contact
+                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
+
+                    while (phoneCursor.moveToNext()) {
+                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+                        contact.putOpt("phone_number", phoneNumber);
+                    }
+
+                    phoneCursor.close();
+
+                    // Query and loop for every email of the contact
+                    Cursor emailCursor = contentResolver.query(EmailCONTENT_URI,	null, EmailCONTACT_ID+ " = ?", new String[] { contact_id }, null);
+
+                    while (emailCursor.moveToNext()) {
+                        email = emailCursor.getString(emailCursor.getColumnIndex(DATA));
+                        contact.putOpt("email", email);
+                    }
+
+                    emailCursor.close();
+                }
+                contactList.add(contact);
+            }
+            mJSONAdapter.updateData(new JSONArray(contactList));
+        }
     }
 
     private void queryBooks(String searchString) {
